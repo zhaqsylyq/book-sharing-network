a735d2e4957dd970870de6d3920ce2b991918a94
zhaqsylyq <zhaksylyk.chalgimbayev@nu.edu.kz>
2024-06-22 00:26:19 +0500

    implemented authorization and authentication with email validation

---
+++ book-network/src/main/java/com/zhaqsylyq/book/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.zhaqsylyq.book.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String roleStudent);
+}
